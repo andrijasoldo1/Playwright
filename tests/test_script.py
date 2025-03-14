3441897d944697b2c8d380691f0439a29e63a149
andrijasoldo1 <andrija.soldo@fsre.sum.ba>
2025-03-14 15:48:09 +0100

    New test with a new method

---
+++ tests/test_script.py
@@ -0,0 +1,18 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://mostar.demo.melon.market/form/application/new/1ddf2594f820438da52a921dd2f5725d/6th-1bd928a4f8fc0b0bf546/67c10f4b-9986-4f94-a90e-4cc6c5e83f6d")
+    page.close()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
