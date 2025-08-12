6a993649362655ddc63fcb7bec39d58b7530a574
andrijasoldo1 <andrija.soldo@fsre.sum.ba>
2025-08-13 00:28:25 +0200

    Dodan novi test test_melon_taskv1

--- tests/test_script.py
+++ tests/test_script.py
@@ -7,7 +7,6 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://mostar.demo.melon.market/form/application/new/1ddf2594f820438da52a921dd2f5725d/6th-1bd928a4f8fc0b0bf546/67c10f4b-9986-4f94-a90e-4cc6c5e83f6d")
-    page.close()
 
     # ---------------------
     context.close()
